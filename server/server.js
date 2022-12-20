f6d79568dd5e2d827ac9ec9d2395629ac38dfe45
shwetabh <shwetabh.srivastava@gmail.com>
2022-12-21 05:28:57 +0530

    21dec

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import Razorpay from "razorpay";
 
 
 export const instance = new Razorpay({
-  key_id: "rzp_test_lUnAJbUwzvvkQf",
-  key_secret: "QmBd7plS0oI4HO1HzNKBQQ2T"
+  key_id: "rzp_live_if5v4dCCSo6KMK",
+  key_secret: "9a6ZAeOFftBXQEFbXQsmi7n5"
 });
 
